f3c926f3fca05a289eb3edf47e475f33d45e5dd9
allamurodxakimov <xakimov2806@gmail.com>
2023-10-15 14:41:25 -0700

    file 9

--- file09.py
+++ file09.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    f=open("data/data09.txt")
+    data=f.read()
+    ls=[]
+    for i in range(len(data)):
+        if data[i].isdigit():
+            ls.append(int(data[i]))
+    mi=ls[0]
+    for i in range(len(ls)):
+        if ls[i]<=mi:
+            mi=ls[i]
+    return mi
+print(main("data09.txt"))
 # Read data from file
