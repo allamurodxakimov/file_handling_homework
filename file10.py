17f158c59ebbbf7d20a49ce5f16b1bea9c600357
allamurodxakimov <xakimov2806@gmail.com>
2023-10-15 14:42:11 -0700

    file 10

--- file10.py
+++ file10.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    f=open("data/data10.txt")
+    data=f.read()
+    l=data.split("\n")
+    ls=[]
+    for i in range(len(l)):
+        ls.append(len(l[i]))
+    ma=ls[0]
+    for i in range(len(ls)):
+        if ma<=ls[i]:
+            ma=ls[i]
+    return ma 
+print(main("data10.py"))
 # Read data from file
