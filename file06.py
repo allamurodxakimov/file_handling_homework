f3e6ef7dee6126bdd9a545271200c168f9e39807
allamurodxakimov <xakimov2806@gmail.com>
2023-10-15 14:40:12 -0700

    file 7

--- file06.py
+++ file06.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    f=open("data/data06.txt")
+    data=f.read()
+    l=data.split()
+    ls=[]
+    for i in range(len(l)):
+        ls.append(len(l[i]))
+    return ls
+print(main("data06.txt"))
 # Read data from file
