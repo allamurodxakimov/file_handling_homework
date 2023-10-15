e3c4e92f991a103fe8e1ddee117b570bf9e2028c
allamurodxakimov <xakimov2806@gmail.com>
2023-10-15 14:39:45 -0700

    file 4

--- file04.py
+++ file04.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    f=open("data/data04.txt")
+    data=f.read()
+    ls=[]
+    for i in range(len(data)):
+        if data[i].isdigit():
+            continue
+        else:
+            ls.append(data[i])
 # Read data from file
+    return ls
+print(main("data04.txt"))
